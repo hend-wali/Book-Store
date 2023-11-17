8cc3d76af6b7d7a2a24783b856ac035566079065
hend <walihend001@gmail.com>
2023-11-17 18:14:53 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const books = [
+  {
+    title: "Fourth Wing",
+    author: "Rebecca Yarros ",
+    img: "./images/Fourth-Wing.jpg",
+    id: 1,
+  },
+  {
+    title: "No Brainer",
+    author: "Jeff Kinney ",
+    img: "./images/No-Brainer.jpg",
+    id: 2,
+  },
+];
+
+function Booklist() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="booklist">
+      <Event />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
   );
 }
+export default Booklist;
 
-export default App;
+const Event = () => {
+  const handleFormInput = () => {
+    return;
+  };
+  const handleButtonClick = () => {
+    return;
+  };
+  const handleFormSubmission = () => {
+    return;
+  };
+  return (
+    <section>
+      <form >
+        <h2> typical Form</h2>
+        <input
+          type="text"
+          name="exemple"
+          onChange={handleFormInput}
+          style={{ margin: "1rem 0" }}
+        />
+        <button type="submit" onClick={handleFormSubmission}> submit </button>
+        <div>
+        <button onClick={handleButtonClick} type="button"> Click me</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+const Book = ({ img, title, author }) => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author} </h4>
+    </article>
+  );
+};

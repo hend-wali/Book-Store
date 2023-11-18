97b55f98a8bbe5f12b86d870d7def8d76353856d
hend <walihend001@gmail.com>
2023-11-18 16:04:10 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,17 @@
-import "./App.css";
-
-const books = [
-  {
-    title: "Fourth Wing",
-    author: "Rebecca Yarros ",
-    img: "./images/Fourth-Wing.jpg",
-    id: 1,
-  },
-  {
-    title: "No Brainer",
-    author: "Jeff Kinney ",
-    img: "./images/No-Brainer.jpg",
-    id: 2,
-  },
-];
+import "./index.css";
+import { books } from "./books";
+import Book from "./Book";
 
 function Booklist() {
   return (
+    <>
+    <h1> amazon best sellers </h1>
     <section className="booklist">
-      <Event />
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+      {books.map((book , index) => {
+        return <Book {...book} key={book.id}  number={index}/>;
       })}
     </section>
+    </>
   );
 }
 export default Booklist;
-
-const Event = () => {
-  const handleFormInput = () => {
-    return;
-  };
-  const handleButtonClick = () => {
-    return;
-  };
-  const handleFormSubmission = () => {
-    return;
-  };
-  return (
-    <section>
-      <form >
-        <h2> typical Form</h2>
-        <input
-          type="text"
-          name="exemple"
-          onChange={handleFormInput}
-          style={{ margin: "1rem 0" }}
-        />
-        <button type="submit" onClick={handleFormSubmission}> submit </button>
-        <div>
-        <button onClick={handleButtonClick} type="button"> Click me</button>
-        </div>
-      </form>
-    </section>
-  );
-};
-
-const Book = ({ img, title, author }) => {
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author} </h4>
-    </article>
-  );
-};
---
+++ src/books.js
@@ -0,0 +1,32 @@
+import img1 from './images/book1.jpg'
+import img2 from './images/book2.jpg'
+import img3 from './images/book3.jpg'
+import img4 from './images/book4.jpg'
+
+export const books = [
+    {
+      title: "Fourth Wing",
+      author: "Rebecca Yarros ",
+      img: img1,
+      id: 1,
+    },
+    {
+      title: "No Brainer",
+      author: "Jeff Kinney ",
+      img: img2,
+      id: 2,
+    },
+    {
+      title: "The Great Disappearance",
+      author: " Dr. David Jeremiah  ",
+      img: img3,
+      id: 3,
+    },
+    {
+      title: "The Heaven & Earth Grocery Store",
+      author: "James McBride",
+      img: img4,
+      id: 4,
+    },
+
+  ];
